d40e90206030e2a59c100abaa153a736b404b2ae
srinivasmarojusrepo <srinivas.maroju@rediffmail.com>
2018-01-07 22:53:18 +0530

    updated client table

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -9,13 +9,23 @@ import { Client } from '../../models/Client';
 })
 export class ClientsComponent implements OnInit {
   clients: any[];
+  totalOwed: number;
 
   constructor(private clientService: ClientService) { }
 
   ngOnInit() {
     this.clientService.getClients().subscribe(clients => {
-      console.log(clients);
+      //console.log(clients);
+      this.clients = clients;
+      this.getTotalOwed();
     });
   }
 
+  getTotalOwed(){
+    let total = 0;
+    for(let i = 0;i < this.clients.length; i++){
+      total += parseFloat(this.clients[i].balance);
+    }
+    this.totalOwed = total;
+  }
 }
