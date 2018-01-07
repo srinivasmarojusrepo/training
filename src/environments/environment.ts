3e5d41233f3d7bf6841f7a4005079685c3bd7b63
srinivasmarojusrepo <srinivas.maroju@rediffmail.com>
2018-01-07 21:49:22 +0530

    angularjs 2 project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCHIgu4XiYp93YE3rEDwHPw2s_Njc4SQL0',
+    authDomain: 'clientpanel-40b46.firebaseapp.com',
+    databaseURL: 'https://clientpanel-40b46.firebaseio.com',
+    projectId: 'clientpanel-40b46',
+    storageBucket: 'clientpanel-40b46.appspot.com',
+    messagingSenderId: '395578311667'
+  }
 };
